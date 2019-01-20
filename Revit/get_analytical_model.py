2849d9fa227bc5308c8142ea123d45d7ffd38456
MarcusAndreasSvensson <marsven95@gmail.com>
2019-01-20 22:14:47 +0100

    Create get_analytical_model.py

---
+++ Revit/get_analytical_model.py
@@ -0,0 +1,9 @@
+"""Collects all analytical lines"""
+
+# import statements-------------------------------------------------
+import Autodesk.Revit.DB as DB
+
+# ------------------------------------------------------------------
+
+doc = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
