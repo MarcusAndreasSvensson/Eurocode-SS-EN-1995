291d58c98640a73e565ca3511c3ad0e1adb12af8
MarcusAndreasSvensson <marsven95@gmail.com>
2019-01-20 22:56:02 +0100

    Update get_analytical_model.py

--- Revit/get_analytical_model.py
+++ Revit/get_analytical_model.py
@@ -1,9 +1,22 @@
 """Collects all analytical lines"""
 
 # import statements-------------------------------------------------
-import Autodesk.Revit.DB as DB
+import clr
+clr.AddReferenceByPartialName('PresentationCore')
+clr.AddReferenceByPartialName("PresentationFramework")
+clr.AddReferenceByPartialName('System')
+clr.AddReferenceByPartialName('System.Windows.Forms')
+
+from Autodesk.Revit import DB
+from Autodesk.Revit import UI
+
+#import Autodesk.Revit.DB as DB
 
 # ------------------------------------------------------------------
 
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
+
+analytical_model_id = DB.ElementId(DB.Get)
+
+doc.
